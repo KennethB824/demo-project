0435c4b1e70df6d89ca3aebb6d4e6c751c05580a
KennethB824 <kenneth.bermudez001@mymdc.net>
2022-07-16 10:42:23 -0400

    trigger practice 2

--- src/test/java/cydeo/StepDefs.java
+++ src/test/java/cydeo/StepDefs.java
@@ -33,7 +33,7 @@ public class StepDefs {
 
     @Then("^I should see the results$")
     public void i_should_see_the_results() throws Throwable {
-        Thread.sleep(2000);
+        Thread.sleep(3000);
         Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("search"));
     }
 
